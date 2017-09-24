255e9fb8f44bbcc5c660cffbe33f8688c63a73b6
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2017-09-25 00:07:27 +0800

    Add reviews on details panel

--- dist/helpers/htmlBuilder.js
+++ dist/helpers/htmlBuilder.js
@@ -26,10 +26,21 @@ var Html = {
     listDiv.appendChild(ul);
   },
 
+  buildReview: function buildReview(reviews) {
+
+    var reviewTemplate = '';
+    reviews.forEach(function (review) {
+      reviewTemplate = '\n        ' + reviewTemplate + '\n        <li class="mdl-list__item mdl-list__item--two-line">\n          <span class="mdl-list__item-primary-content">\n            <i class="material-icons mdl-list__item-avatar">person</i>\n            <span>' + review.author_name + '</span>\n            <span class="mdl-list__item-sub-title">\n              ' + review.text + '\n            </span>\n          </span>\n          <span class="mdl-list__item-secondary-content">\n            <i class="material-icons mdl-color-text--amber-900">star</i>\n          </span>\n        </li>\n      ';
+    });
+    return reviewTemplate;
+  },
+
   buildDetailsPanel: function buildDetailsPanel(data) {
+    console.log('data', data);
     var address = Html.getAddress(data);
+    var placeReviews = Html.buildReview(data.reviews);
     var detailPanelDiv = document.getElementById('detailPanel');
-    var detailPanel = '\n      <div class="mdl-card__title">\n        <h2 class="mdl-card__title-text">' + data.name + '</h2>\n      </div>\n      <div class="mdl-card__supporting-text">\n        ' + address + '\n      </div>\n      <div class="mdl-card__actions mdl-card--border">\n      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"\n        onClick=Directions.get(' + JSON.stringify(data.geometry.location) + ')>\n        Get Directions\n        <i id="tooltipD" class="material-icons mdl-color-text--blue">directions</i>\n        <div class="mdl-tooltip" data-mdl-for="tooltipD">Directions</div>\n      </a>\n    </div>\n      <div class="mdl-card__menu">\n        <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"\n          onClick=Html.hideDetailsPanel()>\n          <i class="material-icons">keyboard_backspace</i>\n        </button>\n      </div>';
+    var detailPanel = '\n      <div class="mdl-card__title">\n        <h2 class="mdl-card__title-text">' + data.name + '</h2>\n      </div>\n      <div class="mdl-card__supporting-text">\n        <strong>Adress:</strong> ' + address + ' <br>\n        <strong>Phone Numer:</strong>: ' + data.international_phone_number + ' / ' + data.formatted_phone_number + ' <br>\n        <strong>Rating:</strong> ' + data.rating + ' <br>\n      </div>\n      <div class="mdl-card__actions mdl-card--border">\n      <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"\n        onClick=Directions.get(' + JSON.stringify(data.geometry.location) + ')>\n        Get Directions\n      </a>\n      <a href="' + data.website + '" target="_blank"\n      class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">\n        Website\n      </a>\n      <a href="' + data.url + '" target="_blank"\n      class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effec">\n        More Details\n      </a>\n      <div class="mdl-card__title">\n        <h2 id="recommendedDiv" class="mdl-card__title-text">Reviews</h2>\n      </div>\n      <div id="reviewPanel">\n        <ul class="demo-list-two mdl-list">\n          ' + placeReviews + '\n        </ul>\n      </div>\n    </div>\n    <div class="mdl-card__menu">\n      <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"\n        onClick=Html.hideDetailsPanel()>\n        <i class="material-icons">keyboard_backspace</i>\n      </button>\n    </div>';
 
     detailPanelDiv.innerHTML = "";
     detailPanelDiv.innerHTML = detailPanel;
